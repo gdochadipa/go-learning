187ed82eab13e75eafa8dcd2a18e59dc791e7115
ochadipa <gede.ocha@igloohome.co>
2025-09-07 14:42:58 +0800

    [go-learn] make example go lambda aws using sam with invoke and event http

--- crud-mongo-go-serverless/internal/app/handler.go
+++ crud-mongo-go-serverless/internal/app/handler.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"github.com/aws/aws-lambda-go/events"
 )
 
 type Event struct {
@@ -13,7 +14,20 @@ type Event struct {
 
 func HandleRequest(ctx context.Context, event Event) (string, error) {
 	log.Printf("handle the request runn")
+
+	// event not show up if using apirest invoke method
 	log.Printf(fmt.Sprintf("Helooo %s", event.Name))
 
 	return fmt.Sprintf("Helooo %s", event.Name), nil
+}
+
+func HttpHandlerRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	// Print the request body to the console for debugging.
+	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	fmt.Printf("Body size = %d.\n", len(request.Body))
+
+	return events.APIGatewayProxyResponse{
+		Body: "heloo world from api",
+		StatusCode: 200,
+	}, nil
 }
