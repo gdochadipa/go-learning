56b7e8d0153de0bc41073d99dac2d9b905b5aa34
ochadipa <gede.ocha@igloohome.co>
2025-11-01 13:02:27 +0800

    push

--- crud-mongo-go-serverless/internal/app/handler.go
+++ crud-mongo-go-serverless/internal/app/handler.go
@@ -2,13 +2,20 @@ package app
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+
 	"github.com/aws/aws-lambda-go/events"
 )
 
 type Event struct {
 	Name string `json:"name"`
+}
+
+// Response is a custom struct to hold our JSON response data.
+type Response struct {
+	Message string `json:"message"`
 }
 
 
@@ -23,11 +30,25 @@ func HandleRequest(ctx context.Context, event Event) (string, error) {
 
 func HttpHandlerRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// Print the request body to the console for debugging.
-	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body size = %d.\n", len(request.Body))
+	// Log the incoming request for debugging
+		log.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+		log.Printf("Request Path: %s\n", request.Path)
 
-	return events.APIGatewayProxyResponse{
-		Body: "heloo world from api",
-		StatusCode: 200,
-	}, nil
+		// Create a response object
+		message := Response{
+			Message: fmt.Sprintf("Hello, World from Go! 🚀 You hit the path: %s", request.Path),
+		}
+
+		// Marshal the Go struct into a JSON string
+		responseBody, err := json.Marshal(message)
+		if err != nil {
+			return events.APIGatewayProxyResponse{Body: "Error marshalling JSON", StatusCode: 500}, err
+		}
+
+		// Return a successful response
+		return events.APIGatewayProxyResponse{
+			Body:       string(responseBody),
+			StatusCode: 200,
+			Headers:    map[string]string{"Content-Type": "application/json"},
+		}, nil
 }
